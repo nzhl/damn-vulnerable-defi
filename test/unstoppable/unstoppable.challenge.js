3b67489edac933abaa2c7c7ac14db11eec26242b
Fred <nzhlfred@gmail.com>
2023-05-15 16:35:28 +0800

    feat: Unstoppable

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -45,6 +45,8 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+      token.connect(player);
+      await token.transfer(vault.address, 10n * 10n ** 18n);
     });
 
     after(async function () {
